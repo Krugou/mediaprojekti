7e7c9ac998d30f2b76d3f7ba042a16810b11555d
Krugou <59641229+Krugou@users.noreply.github.com>
2022-02-14 13:43:28 +0200

    json testausta 3

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 
 
 // index.js
-function searchf() {
 let data="https://iot.fvh.fi/opendata/uiras/70B3D57050004E0E_v1.json";
+function searchf(data1) {
 
-fetch(data).then(response => {
+
+fetch(data1).then(response => {
     if (response.ok) {
       return response.json();
     } else {
@@ -13,11 +14,11 @@ fetch(data).then(response => {
   }).then((jsonData) => {
       let laskejsondata = jsonData.data.length - 1
         console.log(laskejsondata);
-        let paikannimi = jsonData.meta.name;
+      let paikannimi = jsonData.meta.name;
       let aikajsondata = jsonData.data[laskejsondata].time;
       let vedenlampotiladata = jsonData.data[laskejsondata].temp_water;
       let ilmalampotiladata = jsonData.data[laskejsondata].temp_air;
-      console.log(jsonData.meta.name)
+      console.log(paikannimi)
       console.log(aikajsondata)
       console.log(vedenlampotiladata)
       console.log(ilmalampotiladata)
@@ -29,6 +30,6 @@ fetch(data).then(response => {
   });
 }
 window.onload = () => {
-searchf();
+searchf(data);
 
 }
