81ffc588b0adfd18879bcfefc043f716fc652c7b
Krugou <59641229+Krugou@users.noreply.github.com>
2022-02-20 17:28:35 +0200

    update reformatted

--- js/index.js
+++ js/index.js
@@ -1,95 +1,99 @@
-
-
 // index.js
-let data="./json/rannat.json";
+let data = './json/rannat.json';
+
 function urlrandomizer(data1) {
-fetch(data).then(response => {
-  if (response.ok) {
-    return response.json();
-  } else {
-    throw 'HTTP ERROR';
-  }
-}).then((jsonData) => { 
-
-  let datarandomurl = jsonData.data[getRandomIntInclusive(0,14)].url
-  
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-  }
-  
-  searchf(datarandomurl);
-}).catch((error) => {
-  console.log("error")
- });
-}
-function searchf(data1) {
-
-
-fetch(data1).then(response => {
+  fetch(data).then(response => {
     if (response.ok) {
       return response.json();
     } else {
       throw 'HTTP ERROR';
     }
   }).then((jsonData) => {
-      let laskejsondata = jsonData.data.length - 1
-       
-      let lat = jsonData.meta.lat;
-      let lon = jsonData.meta.lon;
-      let minuslat = (lat - 0.09)
-      let minuslon = (lon - 0.09)
-     
-      let latplus = (lat + 0.09)
-      let lonplus = (lon + 0.09)
-      let bbox = minuslon.toFixed(3)+"," +minuslat.toFixed(3) +","+ lonplus.toFixed(3)+"," + latplus.toFixed(3) + ","
-      
-      console.log(bbox)
-     
 
-      let paikannimi = jsonData.meta.name;
-      let aikajsondata = jsonData.data[laskejsondata].time;
-      let vedenlampotiladata = jsonData.data[laskejsondata].temp_water;
-      let ilmalampotiladata = jsonData.data[laskejsondata].temp_air;
+    let datarandomurl = jsonData.data[getRandomIntInclusive(0, 14)].url;
 
+    function getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    }
 
-     let last = document.getElementById("demo").innerHTML  = "<h1>" + paikannimi + "</h1>" +"<p>Vedenlämpötila: "+ vedenlampotiladata+"</p>"+  "<p> Ilmanlämpötila: "+ ilmalampotiladata+"</p>"
-    nouda(bbox)
-
+    searchf(datarandomurl);
   }).catch((error) => {
-   console.log("error")
+    console.log('error');
   });
 }
 
-function nouda(query){
-    
-    let arvo = `https://opendata.fmi.fi/wfs/fin?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::weather::timevaluepair&bbox=${query}epsg::4326&parameters=t2m,ws_10min&crs=EPSG::3067&`
+function searchf(data1) {
 
+  fetch(data1).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw 'HTTP ERROR';
+    }
+  }).then((jsonData) => {
+    let laskejsondata = jsonData.data.length - 1;
 
-console.log(arvo)
-    fetch(arvo)
-  .then(response => response.text())
-  .then((xml) => {
-    console.log(xml)
+    let lat = jsonData.meta.lat;
+    let lon = jsonData.meta.lon;
+    let minuslat = (lat - 0.09);
+    let minuslon = (lon - 0.09);
+
+    let latplus = (lat + 0.09);
+    let lonplus = (lon + 0.09);
+    let bbox = minuslon.toFixed(3) + ',' + minuslat.toFixed(3) + ',' +
+        lonplus.toFixed(3) + ',' + latplus.toFixed(3) + ',';
+
+    console.log(bbox);
+
+    let paikannimi = jsonData.meta.name;
+    let aikajsondata = jsonData.data[laskejsondata].time;
+    let vedenlampotiladata = jsonData.data[laskejsondata].temp_water;
+    let ilmalampotiladata = jsonData.data[laskejsondata].temp_air;
+
+    let last = document.getElementById('demo').innerHTML = '<h1>' + paikannimi +
+        '</h1>' + '<p>Vedenlämpötila: ' + vedenlampotiladata + '</p>' +
+        '<p> Ilmanlämpötila: ' + ilmalampotiladata + '</p>';
+    nouda(bbox);
+
+  }).catch((error) => {
+    console.log('error');
+  });
+}
+
+function nouda(query) {
+
+  let arvo = `https://opendata.fmi.fi/wfs/fin?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::weather::timevaluepair&bbox=${query}epsg::4326&parameters=t2m,ws_10min&crs=EPSG::3067&`;
+
+  console.log(arvo);
+  fetch(arvo).then(response => response.text()).then((xml) => {
+    console.log(xml);
     let parser = new DOMParser();
-    let xmlDOM = parser.parseFromString(xml, "application/xml");
-    let mittaussarja2 = xmlDOM.querySelector('MeasurementTimeseries').querySelectorAll('value'); 
-    let vahenna2 = mittaussarja2.length - 1
-    let aikasarja2 = xmlDOM.querySelector('MeasurementTimeseries').querySelectorAll('time'); 
-    let vahennaaika2 = aikasarja2.length - 1
+    let xmlDOM = parser.parseFromString(xml, 'application/xml');
+    let mittaussarja2 = xmlDOM.querySelector('MeasurementTimeseries').
+        querySelectorAll('value');
+    let vahenna2 = mittaussarja2.length - 1;
+    let aikasarja2 = xmlDOM.querySelector('MeasurementTimeseries').
+        querySelectorAll('time');
+    let vahennaaika2 = aikasarja2.length - 1;
     let paikka3 = xmlDOM.querySelectorAll('name');
-    console.log(paikka3[1].childNodes[0])
-    console.log(mittaussarja2)
-    console.log(vahenna2)
-    console.log(mittaussarja2[vahenna2])
-    console.log(aikasarja2[vahennaaika2])
-  
-    let tulos = document.getElementById("tulos").innerText = "sijainnissa: " + paikka3[1].childNodes[0].textContent + " on lämpötila " + mittaussarja2[vahenna2].textContent + " celsiusta" + " kello oli järjestelmän mukaan: " + aikasarja2[vahennaaika2].textContent
-    console.log(tulos)
-})
-}
-window.onload = () => {
-urlrandomizer(data);
+    console.log(paikka3[1].childNodes[0]);
+    console.log(mittaussarja2);
+    console.log(vahenna2);
+    console.log(mittaussarja2[vahenna2]);
+    console.log(aikasarja2[vahennaaika2]);
 
+    let tulos = document.getElementById('tulos').innerText = 'sijainnissa: ' +
+        paikka3[1].childNodes[0].textContent + ' on lämpötila ' +
+        mittaussarja2[vahenna2].textContent + ' celsiusta' +
+        ' kello oli järjestelmän mukaan: ' +
+        aikasarja2[vahennaaika2].textContent;
+    console.log(tulos);
+  });
 }
+
+window.onload = () => {
+  urlrandomizer(data);
+
+};
--- nodetwitterbotti/server.js
+++ nodetwitterbotti/server.js
@@ -1,56 +1,63 @@
 import 'dotenv/config';
-import { TwitterApi } from 'twitter-api-v2';
+import {TwitterApi} from 'twitter-api-v2';
 
 import fetch from 'node-fetch';
 
-let data="https://users.metropolia.fi/~aleksino/mediaprojekti/json/rannat.json";
+let data = 'https://users.metropolia.fi/~aleksino/mediaprojekti/json/rannat.json';
 const userClient = new TwitterApi({
-    appKey: process.env.TWITTER_API_KEY,
-    appSecret: process.env.TWITTER_API_SECRET,
-    accessToken: process.env.TWITTER_ACCESS_TOKEN,
-    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  appKey: process.env.TWITTER_API_KEY,
+  appSecret: process.env.TWITTER_API_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 const tweet = async () => {
-    
-    const response = await fetch(data);
-const fetchDataJson = await response.json();
-var mainTempdata = [];
-var mainplacedata = [];
-var mainilmalampotiladata = [];
-var mainaikajsondata = [];
-for (let i = 0; i < 14; i++) {
+
+  const response = await fetch(data);
+  const fetchDataJson = await response.json();
+  var mainTempdata = [];
+  var mainplacedata = [];
+  var mainilmalampotiladata = [];
+  var mainaikajsondata = [];
+  for (let i = 0; i < 14; i++) {
     const response = await fetch(fetchDataJson.data[i].url);
     const fetchDataJson2 = await response.json();
-    let laskejsondata = fetchDataJson2.data.length - 1
+    let laskejsondata = fetchDataJson2.data.length - 1;
     let paikannimi = fetchDataJson2.meta.name;
     let aikajsondata = fetchDataJson2.data[laskejsondata].time;
     let vedenlampotiladata = fetchDataJson2.data[laskejsondata].temp_water;
     let ilmalampotiladata = fetchDataJson2.data[laskejsondata].temp_air;
-    mainTempdata.push(vedenlampotiladata)
-    mainplacedata.push(paikannimi)
-    mainilmalampotiladata.push(ilmalampotiladata)
-    mainaikajsondata.push(aikajsondata)
-    console.log(paikannimi)
-    console.log(aikajsondata)
-    console.log(vedenlampotiladata)
-    console.log(ilmalampotiladata )
+    mainTempdata.push(vedenlampotiladata);
+    mainplacedata.push(paikannimi);
+    mainilmalampotiladata.push(ilmalampotiladata);
+    mainaikajsondata.push(aikajsondata);
+    console.log(paikannimi);
+    console.log(aikajsondata);
+    console.log(vedenlampotiladata);
+    console.log(ilmalampotiladata);
   }
-const max = Math.max(...mainTempdata);
-const index = mainTempdata.indexOf(max);
-    let paikka = mainplacedata[index]
-    let vedenlampotila = mainTempdata[index]
-    let ilmalampotila = mainilmalampotiladata[index]
-    let aika =new Date(mainaikajsondata[index]).valueOf();
-    console.log(paikka)
-    console.log(vedenlampotila)
-    console.log(ilmalampotila)
-    console.log(aika)
-    try {
-       await userClient.v2.tweet('Kuumin uimaveden lämpötila on paikassa: ' + paikka + ' asteita on ' + vedenlampotila+ ' \xB0C  ja '+ 'Ilman lämpötila on '+ilmalampotila+ ' \xB0C ')
-        console.log('tweettaus onnistui '+ 'twiitti: Kuumin uimaveden lämpötila on paikassa: ' + paikka + ' asteita on ' + vedenlampotila+ ' \xB0C  ja '+ 'Ilman lämpötila on '+ilmalampotila+ ' \xB0C ')
-     } catch (e) {
-         console.error(e)
-     }
-}
-tweet()
-setInterval(tweet,1800000)
+  const max = Math.max(...mainTempdata);
+  const index = mainTempdata.indexOf(max);
+  let paikka = mainplacedata[index];
+  let vedenlampotila = mainTempdata[index];
+  let ilmalampotila = mainilmalampotiladata[index];
+  let aika = new Date(mainaikajsondata[index]);
+  console.log(paikka);
+  console.log(vedenlampotila);
+  console.log(ilmalampotila);
+  console.log(aika.toString());
+  try {
+    await userClient.v2.tweet(
+        'Kuumin uimaveden lämpötila on paikassa: ' + paikka + ' asteita on ' +
+        vedenlampotila + ' \xB0C  ja ' + 'Ilman lämpötila on ' + ilmalampotila +
+        ' \xB0C ' + 'kello: ' + aika.toLocaleTimeString('fi-FI'));
+    console.log('tweettaus onnistui ' +
+        'twiitti: Kuumin uimaveden lämpötila on paikassa: ' + paikka +
+        ' asteita on ' + vedenlampotila + ' \xB0C  ja ' +
+        'Ilman lämpötila on ' + ilmalampotila + ' \xB0C ' + 'kello: ' +
+        aika.toLocaleTimeString('fi-FI'));
+  } catch (e) {
+    console.error(e);
+  }
+};
+tweet();
+setInterval(tweet, 1800000);
