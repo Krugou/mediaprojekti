05d7668d92c094cde046504ddd1094424de740ce
nygator <juku_man@hotmail.com>
2022-03-10 20:14:26 +0200

    Reversed to an older version

--- nodetwitterbotti/server.js
+++ nodetwitterbotti/server.js
@@ -20,9 +20,9 @@ const tweet = async () => {
   let airTemperatureData = [];
   let timeTakenData = [];
   let currentDate = new Date();
- // console.log(currentDate.getTime());
- // säädatan haku
-  for (let i = 0; i < fetchDataJson.beaches.length; i++) { 
+  // console.log(currentDate.getTime());
+  // säädatan haku
+  for (let i = 0; i < fetchDataJson.beaches.length; i++) {
     const response = await fetch("https://iot.fvh.fi/opendata/uiras/"+fetchDataJson.beaches[i].url+".json");
     const fetchDataJson2 = await response.json();
     const laskejsondata = fetchDataJson2.data.length - 1;
@@ -31,23 +31,23 @@ const tweet = async () => {
     console.log("nykyinen aika - 7200000: "+(currentDate.getTime() - 7200000))
     console.log("nykyinen aika: "+currentDate.getTime())
     if ( jsonaika.getTime() > (currentDate.getTime() - 7200000) && jsonaika.getTime() < currentDate.getTime() ) {
-    let paikannimi = fetchDataJson2.meta.name;
-    let aikajsondata = fetchDataJson2.data[laskejsondata].time;
-    let waterTemperaturedata = fetchDataJson2.data[laskejsondata].temp_water;
-    let airTemperaturedata = fetchDataJson2.data[laskejsondata].temp_air;
-   
-    temperatureData.push(waterTemperaturedata);
-    placeNameData.push(paikannimi);
-    airTemperatureData.push(airTemperaturedata);
-    timeTakenData.push(aikajsondata);
-    console.log(paikannimi);
-    console.log(aikajsondata);
-    console.log(waterTemperaturedata);
-    console.log(airTemperaturedata);
-  } else {
-    continue;
-    
-  }
+      let paikannimi = fetchDataJson2.meta.name;
+      let aikajsondata = fetchDataJson2.data[laskejsondata].time;
+      let waterTemperaturedata = fetchDataJson2.data[laskejsondata].temp_water;
+      let airTemperaturedata = fetchDataJson2.data[laskejsondata].temp_air;
+
+      temperatureData.push(waterTemperaturedata);
+      placeNameData.push(paikannimi);
+      airTemperatureData.push(airTemperaturedata);
+      timeTakenData.push(aikajsondata);
+      console.log(paikannimi);
+      console.log(aikajsondata);
+      console.log(waterTemperaturedata);
+      console.log(airTemperaturedata);
+    } else {
+      continue;
+
+    }
   }
   const max = Math.max(...temperatureData);
   const index = temperatureData.indexOf(max);
@@ -61,15 +61,15 @@ const tweet = async () => {
   console.log(timeTaken.toLocaleTimeString('fi-FI'));
   //tweetataan
   try {
-   await userClient.v2.tweet(
-       'Kuumin uimaveden lämpötila on paikassa: ' + placeName + ' asteita on ' +
+    await userClient.v2.tweet(
+        'Kuumin uimaveden lämpötila on paikassa: ' + placeName + ' asteita on ' +
         waterTemperature + ' \xB0C  ja ' + 'Ilman lämpötila on ' + airTemperature +
         ' \xB0C ' + 'kello: ' + timeTaken.toLocaleTimeString('fi-FI'));
-   console.log('tweettaus onnistui ' +
-       'twiitti: Kuumin uimaveden lämpötila on paikassa: ' + placeName +
-      ' asteita on ' + waterTemperature + ' \xB0C  ja ' +
-     'Ilman lämpötila on ' + airTemperature + ' \xB0C ' + 'kello: ' +
-     timeTaken.toLocaleTimeString('fi-FI'));
+    console.log('tweettaus onnistui ' +
+        'twiitti: Kuumin uimaveden lämpötila on paikassa: ' + placeName +
+        ' asteita on ' + waterTemperature + ' \xB0C  ja ' +
+        'Ilman lämpötila on ' + airTemperature + ' \xB0C ' + 'kello: ' +
+        timeTaken.toLocaleTimeString('fi-FI'));
   } catch (e) {
     console.error(e);
   }
